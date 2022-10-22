a4be81440decb6ab66895e03f4e77c458072ef86
chixiyu <2980364872@qq.com>
2022-10-22 21:32:40 +0800

    Added StudentTreeDiagramView

---
+++ src/views/StudentTreeDiagramView.vue
@@ -0,0 +1,29 @@
+<template>
+  <a-alert :message="`You selected date: ${selectedValue && selectedValue.format('YYYY-MM-DD')}`" />
+  <a-calendar :value="date" @select="onSelect" @panelChange="onPanelChange" />
+</template>
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import dayjs, { Dayjs } from 'dayjs';
+
+export default defineComponent({
+  setup() {
+    const date = ref(dayjs());
+    const selectedValue = ref(dayjs());
+
+    const onSelect = (value: Dayjs) => {
+      date.value = value;
+      selectedValue.value = value;
+    };
+    const onPanelChange = (value: Dayjs) => {
+      date.value = value;
+    };
+    return {
+      date,
+      selectedValue,
+      onSelect,
+      onPanelChange,
+    };
+  },
+});
+</script>
